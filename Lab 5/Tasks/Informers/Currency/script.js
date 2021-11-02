0f667a33e4ebe4145f299f77e454d9d0c67b3804
Oleksandr Ruhliadev <sasharuhliadev2@gmail.com>
2021-11-02 15:53:07 +0200

    Added tema, met, multimedia html; changed currency

--- Lab 5/Tasks/Informers/Currency/script.js
+++ Lab 5/Tasks/Informers/Currency/script.js
@@ -1,4 +1,3 @@
-$('.jic').inlineCurrency({
-    "currency": "USD",
-    "convertTo": "EUR, PLN",
-  });
+var iframe = '<ifra'+'me width="500" fram'+'eborder="0" src="https://informer.minfin.com.ua/ua/gen/region/0/?color=blue" vspace="0" scrolling="no" hspace="0" allowtransparency="true"style="width:500px;ove'+'rflow:hidden;"></iframe>';
+var cl = 'minfin-informer-m1Fn-region';
+document.getElementById(cl).innerHTML = iframe;
