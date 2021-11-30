e0555d8e1cc6abeb50c432202e4cd73c81e23837
Oleksandr Ruhliadev <sasharuhliadev2@gmail.com>
2021-11-30 16:55:04 +0200

    Modified form

--- Lab 7/Code/Ruhliadev/add_data.php
+++ Lab 7/Code/Ruhliadev/add_data.php
@@ -1,9 +1,9 @@
 <?php
     function checkQuery($query, $data, $connection){
         if ($connection->query($query) === FALSE)
-            echo "Error: " . $query . "<br>" . $connection->error . "<br>";
+            echo "<span class='error'>" . "Error: " . $query . "<br>" . $connection->error . "<br>" . "</span>";
         else
-            echo "<p>"."Data about $data was successfully add to DB!"."<p>";
+            echo "<p>"."Data about $data was successfully add to DB!"."</span>";
     }
 
     function addPhotographer($fullName, $dateOfBirth, $connection){
@@ -13,7 +13,7 @@
             checkQuery($query, $fullName, $connection);
         }
         else
-            echo "<p>"."Data about photographer was not add to DB! Full name and date of birth must be initialize!"."<p>";
+            echo "<span class='error'>"."Data about photographer was not add to DB! Full name and date of birth must be initialize!"."</span>";
     }
 
     function addStudio($studioName, $studioAdress, $connection){
@@ -23,7 +23,7 @@
             checkQuery($query, $studioName, $connection);
         }
         else
-            echo "<p>"."Data about studio was not add to DB! Studio name and adress must be initialize!"."<p>";
+            echo "<span class='error'>"."Data about studio was not add to DB! Studio name and adress must be initialize!"."</span>";
     }
 
     function addStorage($storageName, $storageAdress, $connection){
@@ -33,27 +33,31 @@
             checkQuery($query, $storageName, $connection);
         }
         else
-            echo "<p>"."Data about storage was not add to DB! Storage name and adress must be initialize!"."<p>";
+            echo "<span class='error'>"."Data about storage was not add to DB! Storage name and adress must be initialize!"."</span>";
     }
 
-    function checkIfEmpty($data){
-        if(empty($data))
-            return 'null';
-        else
-            return $data;
+    function checkIfEmptyNumber($number){
+        $number = $number == NULL ? 'NULL' : $number;
+        return $number;
+    }
+
+    function checkIfEmptyData($data){
+        $data = $data == NULL ? 'NULL' : "'$data'";
+        return $data;
     }
 
     function addPhoto($photoName, $photoTema, $dateOfCreating, $description, $photoStudioID, $photographerID, $storageID, $connection){
         $query = "";
         if(!empty($photoName) and !empty($photoTema) and !empty($storageID)){
-            $photoStudioID = checkIfEmpty($photoStudioID);
-            $photographerID = checkIfEmpty($photographerID);
-            $query = "INSERT IGNORE INTO photo (PhotoName, Tema, DateOfCreating, Description, PhotoStudioID, PhotographerID, StorageID)";
-            $query .= " VALUES (\"$photoName\", \"$photoTema\", " . ($dateOfCreating==NULL ? "NULL" : "'$dateOfCreating'") . ",";
-            $query .= " " . ($description==NULL ? "NULL" : "'$description'") . ", $photoStudioID, $photographerID, $storageID);";
+            $photoStudioID = checkIfEmptyNumber($photoStudioID);
+            $photographerID = checkIfEmptyNumber($photographerID);
+            $dateOfCreating = checkIfEmptyData($dateOfCreating);
+            $description = checkIfEmptyData($description);
+            $query = "INSERT IGNORE INTO photo (PhotoName, Tema, DateOfCreating, Description, PhotoStudioID, PhotographerID, StorageID) ";
+            $query .= "VALUES (\"$photoName\", \"$photoTema\", $dateOfCreating,  $description, $photoStudioID, $photographerID, $storageID);";
             checkQuery($query, $photoName, $connection);
         }
         else
-            echo "<p>"."Data about photo was not add to DB! Photo name, tema and storageID must be initialize!"."<p>";
+            echo "<span class='error'>"."Data about photo was not add to DB! Photo name, tema and storageID must be initialize!"."</span>";
     }
 ?>
--- Lab 7/Code/Ruhliadev/form.php
+++ Lab 7/Code/Ruhliadev/form.php
@@ -3,94 +3,89 @@
 <head>
     <title>LAB7</title>
     <meta charset="utf-8">
-    <style> .error {color: #FF0000;} </style>
+    <?php include 'add_data.php'; ?>
+    <style> .error {color: #f00;}  div {display: none;} </style>
 </head>
 <body>
-    <div>
-        <h2>Photographer</h2>
-        <p><span class="error">* required field</span></p>
-        <form method="post" name="reg" action="">
-            <p>Full name: <input type="text" name="fullName" size="40"><span class="error"> *</span></p>
-            <p>Date of birth: <input type="text" name="dateOfBirth" size="40"><span class="error"> *</span></p>
-            <input type="submit" name="submit" >
-        </form>
+    <header>
+        <nav  style="display: flex; justify-content: space-around;">
+            <button onclick="show(0)">Add photographer</button>
+            <button onclick="show(1)">Add studio</button>
+            <button onclick="show(2)">Add storage</button>
+            <button onclick="show(3)">Add photo</button>
+        </nav>
+    </header>
+    <main>
+        <script>
+            function show(index) {
+                var forms = document.querySelectorAll("div");
+                if (forms[index].style.display === "none") {
+                    forms[index].style.display = "block";
+                } else {
+                    forms[index].style.display = "none";
+                }
+            }
+        </script>
+        <div>
+            <h2>Photographer</h2>
+            <p><span class="error">* required field</span></p>
+            <form method="post" name="reg" action="">
+                <p>Full name: <input type="text" name="fullName" size="40"><span class="error"> *</span></p>
+                <p>Date of birth: <input type="text" name="dateOfBirth" size="40"><span class="error"> *</span></p>
+                <input type="submit" name="submit" >
+            </form>
+        </div>
+        <div>
+            <h2>Studio</h2>
+            <p><span class="error">* required field</span></p>
+            <form method="post" name="reg" action="">
+                <p>Name: <input type="text" name="studioName" size="40"><span class="error"> *</span></p>
+                <p>Adress: <input type="text" name="studioAdress" size="40"><span class="error"> *</span></p>
+                <input type="submit" name="submit" >
+            </form>
+        </div>
+        <div>
+            <h2>Storage</h2>
+            <p><span class="error">* required field</span></p>
+            <form method="post" name="reg" action="">
+                <p>Name: <input type="text" name="storageName" size="40"><span class="error"> *</span></p>
+                <p>Adress: <input type="text" name="storageAdress" size="40"><span class="error"> *</span></p>
+                <input type="submit" name="submit" >
+            </form>
+        </div>
+        <div>
+            <h2>Photo</h2>
+            <p><span class="error">* required field</span></p>
+            <form method="post" name="reg" action="">
+                <p>Name: <input type="text" name="photoName" size="40"><span class="error"> *</span></p>
+                <p>Tema: <input type="text" name="photoTema" size="40"><span class="error"> *</span></p>
+                <p>Date of creating: <input type="text" name="dateOfCreating" size="40"></p>
+                <p>Description: <input type="text" name="description" size="40"></p>
+                <p>Studio id: <input type="number" name="photoStudioID" size="40"></p>
+                <p>Photographer id: <input type="number" name="photographerID" size="40"></p>
+                <p>Storage id: <input type="number" name="storageID" size="40"><span class="error"> *</span></p>
+                <input type="submit" name="submit" >
+            </form>
+        </div>
         <?php
-            include 'add_data.php';
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                echo "<br>";
                 $connection = new mysqli("localhost", "root", null, "photoarchive") or die ("Could not connect:");
-                if(!empty($_POST["fullName"]) and !empty($_POST["dateOfBirth"])){
+                if(isset($_POST["fullName"]) or isset($_POST["dateOfBirth"])){
                     addPhotographer($_POST["fullName"], $_POST["dateOfBirth"], $connection);
                 }
-                else
-                    echo "<span class='error'>"."Full name and date of birth must be initialize!"."<span>";
-                $connection->close();
-            }
-        ?>
-    </div>
-    <div>
-        <h2>Studio</h2>
-        <p><span class="error">* required field</span></p>
-        <form method="post" name="reg" action="">
-            <p>Name: <input type="text" name="studioName" size="40"><span class="error"> *</span></p>
-            <p>Adress: <input type="text" name="studioAdress" size="40"><span class="error"> *</span></p>
-            <input type="submit" name="submit" >
-        </form>
-        <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $connection = new mysqli("localhost", "root", null, "photoarchive") or die ("Could not connect:");
-                if(!empty($_POST["studioName"])){
+                if(isset($_POST["studioName"]) or isset($_POST["studioAdress"])){
                     addStudio($_POST["studioName"], $_POST["studioAdress"], $connection);
                 }
-                else
-                    echo "<span class='error'>"."Studio name and adress must be initialize!"."<span>";
-                $connection->close();
-            }
-        ?>
-    </div>
-    <div>
-        <h2>Storage</h2>
-        <p><span class="error">* required field</span></p>
-        <form method="post" name="reg" action="">
-            <p>Name: <input type="text" name="storageName" size="40"><span class="error"> *</span></p>
-            <p>Adress: <input type="text" name="storageAdress" size="40"><span class="error"> *</span></p>
-            <input type="submit" name="submit" >
-        </form>
-        <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $connection = new mysqli("localhost", "root", null, "photoarchive") or die ("Could not connect:");
-                if(!empty($_POST["storageName"])){
+                if(isset($_POST["storageName"]) or isset($_POST["storageAdress"])){
                     addStorage($_POST["storageName"], $_POST["storageAdress"], $connection);
                 }
-                else
-                    echo "<span class='error'>"."Storage name and adress must be initialize!"."<span>";
-                $connection->close();
-            }
-        ?>
-    </div>
-    <div>
-        <h2>Photo</h2>
-        <p><span class="error">* required field</span></p>
-        <form method="post" name="reg" action="">
-            <p>Name: <input type="text" name="photoName" size="40"><span class="error"> *</span></p>
-            <p>Tema: <input type="text" name="photoTema" size="40"><span class="error"> *</span></p>
-            <p>Date of creating: <input type="text" name="dateOfCreating" size="40"></p>
-            <p>Description: <input type="text" name="description" size="40"></p>
-            <p>Studio id: <input type="number" name="photoStudioID" size="40"></p>
-            <p>Photographer id: <input type="number" name="photographerID" size="40"></p>
-            <p>Storage id: <input type="number" name="storageID" size="40"><span class="error"> *</span></p>
-            <input type="submit" name="submit" >
-        </form>
-        <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $connection = new mysqli("localhost", "root", null, "photoarchive") or die ("Could not connect:");
-                if(!empty($_POST["photoName"])){
+                if(isset($_POST["photoName"]) or isset($_POST["photoTema"]) or isset($_POST["storageID"])){
                     addPhoto($_POST["photoName"], $_POST["photoTema"], $_POST["dateOfCreating"], $_POST["description"], $_POST["photoStudioID"], $_POST["photographerID"], $_POST["storageID"], $connection);
                 }
-                else
-                    echo "<span class='error'>"."Photo name, tema and storageID must be initialize!"."<span>";
                 $connection->close();
             }
         ?>
-    </div>
+    </main>
 </body>
 </html>
